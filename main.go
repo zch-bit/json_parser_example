0baee41be4fe3cf71848b659fdb39ad5b361f70a
zch <chenzhixi@gmail.com>
2022-04-20 15:05:02 -0700

    Mock DB

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 type User struct {
+	ID       uint   `json:"id"`
 	Name     string `json:"name"`
 	LastName string `json:"last_name"`
 	Age      string `json:"age"`
@@ -19,6 +20,8 @@ type User struct {
 		Brand string `json:"brand"`
 	} `json:"like"`
 }
+
+var db map[uint]User
 
 func main() {
 	filepath := "./data.json"
@@ -33,6 +36,34 @@ func main() {
 		fmt.Println("unmarshal json error", err.Error())
 	}
 
-	fmt.Printf("users=%v \n", users)
+	fmt.Printf("get all users=%v \n", users)
 
+	fmt.Println("get all into DB")
+	db = map[uint]User{}
+	initDB(db, users)
+
+	fmt.Printf("GetAll = %v \n", GetAll(db))
+	u, _ := Get(db, 4107)
+	fmt.Printf("Get id = %v \n", u)
+}
+
+func initDB(db map[uint]User, users []User) {
+	for _, user := range users {
+		db[user.ID] = user
+	}
+}
+
+func GetAll(db map[uint]User) []User {
+	res := []User{}
+	for _, val := range db {
+		res = append(res, val)
+	}
+	return res
+}
+
+func Get(db map[uint]User, id uint) (User, error) {
+	if val, ok := db[id]; ok {
+		return val, nil
+	}
+	return User{}, fmt.Errorf("no such record")
 }
